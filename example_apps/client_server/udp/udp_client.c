53fb37d29da97ccf6a9e733f4c76bc8241abfeed
gurkaran <gurkaran@c84e6f64-2931-0410-b4d5-fcb4a352a0cd>
2014-02-09 02:34:05 +0000

    Some refactoring and comments for udp server/client programs

--- example_apps/client_server/udp/udp_client.c
+++ example_apps/client_server/udp/udp_client.c
@@ -15,30 +15,29 @@
 
 #include "cnxn_constants.h"
 
-int main(int argc, char *argv[])
-{
-	int sockfd;
-	struct addrinfo hints, *servinfo, *p;
-	int rv;
-	int numbytes;
-
-	if (argc != 2) {
-		fprintf(stderr,"usage: %s hostname\n", argv[0]);
-		exit(1);
-	}
-
+// populate the servinfo structure which is a 
+// pointer to a linked list of useful network
+// information about the host
+//
+// returns non-zero error code if failed,
+//                  and zero otherwise.
+int set_servinfo(const char* hostname, struct addrinfo** servinfo) {
+	struct addrinfo hints;	
 	memset(&hints, 0, sizeof hints);
 	hints.ai_family = AF_INET; // IPv4
 	hints.ai_socktype = SOCK_DGRAM; // UDP Datagram sockets
 
-	if ((rv = getaddrinfo(argv[1], CNXN_PORT, &hints, &servinfo)) != 0) {
-		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
-		return 1;
-	}
-
+	return getaddrinfo(hostname, CNXN_PORT, &hints, servinfo);
+}
+// use the servinfo linkedlist to get a socket to use
+// set sockfd to the socket that we will use
+//
+// returns NULL if failed, the addrinfo of the socket if succeded
+struct addrinfo* get_socket_fd(struct addrinfo* servinfo, int* sockfd) {
+	struct addrinfo* p;
 	// loop through all the results and make a socket
 	for(p = servinfo; p != NULL; p = p->ai_next) {
-		if ((sockfd = socket(p->ai_family, p->ai_socktype,
+		if ((*sockfd = socket(p->ai_family, p->ai_socktype,
 						p->ai_protocol)) == -1) {
 			perror("udp_client: socket");
 			continue;
@@ -46,27 +45,13 @@ int main(int argc, char *argv[])
 
 		break;
 	}
+	return p;
+}
 
-	if (p == NULL) {
-		fprintf(stderr, "udp_client: failed to bind socket\n");
-		return 2;
-	}
-
-	int packet_size = 0;
-	for (packet_size = INIT_PACKET_SIZE; packet_size <= MAX_PACKET_SIZE; packet_size += PACKET_SIZE_INCREMENT) {
-		char msg[packet_size];
-		memset(msg, 'a', packet_size-1);
-		msg[packet_size] = '\0';
-		if ((numbytes = sendto(sockfd, msg, packet_size, 0,
-						p->ai_addr, p->ai_addrlen)) == -1) {
-			perror("udp_client: sendto");
-			exit(1);
-		}
-		printf("udp_client: sent %d bytes to %s\n", numbytes, argv[1]);
-	}
-
-	// Send the shudown message numberous times to ensure
-	// that it gets through to the server
+// Send the shudown message numberous times to ensure
+// that it gets through to the server
+void send_shutdown_message(int sockfd, const struct addrinfo* p) {
+	int numbytes = -1;
 	int i = 0;
 	for (i = 0; i < 10; i++) {
 		if ((numbytes = sendto(sockfd, SHUTDOWN_MESSAGE, strlen(SHUTDOWN_MESSAGE), 0,
@@ -75,9 +60,57 @@ int main(int argc, char *argv[])
 			exit(1);
 		}
 	}
-
 	printf("udp_client: sent \"%s\" to shutdown server\n", SHUTDOWN_MESSAGE);
+}
+
+int main(int argc, char *argv[])
+{
+	struct addrinfo* p;
+	int sockfd;
+	struct addrinfo *servinfo;
+	int rv;
+	int numbytes;
+	const char* hostname = argv[1];
+
+	if (argc != 2) {
+		fprintf(stderr,"usage: %s hostname\n", argv[0]);
+		exit(1);
+	}
+
+	if ((rv = set_servinfo(hostname, &servinfo)) != 0) {
+		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
+		return 1;
+	}
+
+	if ((p = get_socket_fd(servinfo, &sockfd)) == NULL) {
+		fprintf(stderr, "udp_client: failed to bind socket\n");
+		return 2;
+	}
+	
+	// do not need servinfo anymore
 	freeaddrinfo(servinfo);
+
+	int packet_size = 0;
+	for (packet_size = INIT_PACKET_SIZE;
+			packet_size <= MAX_PACKET_SIZE;
+			packet_size += PACKET_SIZE_INCREMENT) {
+
+		// Fill up a buffer of size packet_size with
+		// 'a's to send a packet to server
+		char msg[packet_size];
+		memset(msg, 'a', packet_size-1);
+		msg[packet_size] = '\0';
+
+		// send the 'packet' just made to the server
+		if ((numbytes = sendto(sockfd, msg, packet_size, 0,
+						p->ai_addr, p->ai_addrlen)) == -1) {
+			perror("udp_client: sendto");
+			exit(1);
+		}
+		printf("udp_client: sent %d bytes to %s\n", numbytes, hostname);
+	}
+
+	send_shutdown_message(sockfd, p);
 	close(sockfd);
 	return 0;
 }
--- example_apps/client_server/udp/udp_server.c
+++ example_apps/client_server/udp/udp_server.c
@@ -16,6 +16,12 @@
 #include "cnxn_constants.h"
 
 
+// populate the servinfo structure which is a 
+// pointer to a linked list of useful network
+// information about the host
+//
+// returns non-zero error code if failed,
+//                  and zero otherwise.
 int set_servinfo(struct addrinfo** servinfo) {
 	struct addrinfo hints;
 	memset(&hints, 0, sizeof hints);
@@ -26,6 +32,10 @@ int set_servinfo(struct addrinfo** servinfo) {
 	return getaddrinfo(NULL, CNXN_PORT, &hints, servinfo);
 }
 
+// use the servinfo linkedlist to get a socket to bind to
+// set sockfd to the socket that we binded to
+//
+// returns NULL if failed, the addrinfo of what we binded to if succeded
 struct addrinfo* get_socket_fd(struct addrinfo* servinfo, int* sockfd) {
 	struct addrinfo* p;
 	// loop through all the results and bind to the first we can
@@ -67,24 +77,28 @@ int main(void)
 	char buf[MAX_PACKET_SIZE];
 	socklen_t addr_len;
 	char s[INET6_ADDRSTRLEN];
-	const char* shutdown_str = "done";
+	int shutdown = 1;
 
 	if ((rv = set_servinfo(&servinfo)) != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(rv));
 		return 1;
 	}
-	
+
 	if (get_socket_fd(servinfo, &sockfd) == NULL) {
 		fprintf(stderr, "udp_server: failed to bind socket\n");
 		return 2;
 	}
 
+	// no longer need servinfo
 	freeaddrinfo(servinfo);
 
-	printf("udp_server: waiting to recvfrom...\n");
-	int done = 1;
 	addr_len = sizeof their_addr;
-	do {    
+	printf("udp_server: waiting to recvfrom...\n");
+
+	// Main loop that runs until the shutdown message is received
+	// The shutdown message is defined in cnxn_constants.h
+	do {   
+		// wait and receive messages on sockfd 
 		if ((numbytes = recvfrom(sockfd, buf, MAX_PACKET_SIZE-1 , 0,
 						(struct sockaddr *)&their_addr, &addr_len)) == -1) {
 			perror("recvfrom");
@@ -97,11 +111,14 @@ int main(void)
 					s, sizeof s));
 		printf("udp_server: packet is %d bytes long\n", numbytes);
 		buf[numbytes] = '\0';
-		// printf("udp_server: packet contains \"%s\"\n", buf);
-		done = strncmp(buf, shutdown_str, strlen(shutdown_str) - 1);
+
+		// check if the received message was the shutdown message
+		shutdown = strncmp(buf, SHUTDOWN_MESSAGE, strlen(SHUTDOWN_MESSAGE) - 1);
+
+		// reset the buffer for the next message
 		memset(buf, 0x0, numbytes);
-	} while (done != 0);
-	printf("udp_server: received \"%s\", shutting down server\n", shutdown_str);
+	} while (shutdown != 0);
+	printf("udp_server: received \"%s\", shutting down server\n", SHUTDOWN_MESSAGE);
 	close(sockfd);
 	return 0;
 }
